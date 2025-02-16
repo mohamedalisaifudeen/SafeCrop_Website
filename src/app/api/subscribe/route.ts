68ddd148f48e94609abebd6cfe8e5ac58cd02ade
Mohamed Ali <mohamedali@Mohameds-MacBook-Air.local>
2025-02-16 17:22:32 +0530

    final

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -1,13 +1,12 @@
-
-import type { NextApiResponse } from 'next';
 import { NextResponse } from 'next/server';
 
-export async function POST(req: Request, res: NextApiResponse) {
+export async function POST(req: Request) {
   try {
     const { email } = await req.json();
 
     if (!email) {
-      return res.status(400).json({ error: 'Email is required' });
+      return NextResponse.json({ error: 'Email is required' }, { status: 400 });
+
     }
 
 
